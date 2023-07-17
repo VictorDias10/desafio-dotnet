3d207e88ee460a2cd9017e96e8d53888947f8f7e
Victor <vdias201313@gmail.com>
2023-07-16 22:50:11 -0300

    feat: teste6

---
+++ src/Teste4.WindowsForms/scripts/CreateTable .sql
@@ -0,0 +1,8 @@
+﻿  CREATE TABLE `escola`.`alunos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `notaPrimeiroSemestre` FLOAT NOT NULL,
+  `notaSegundoSemestre` FLOAT NOT NULL,
+  `ano` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
