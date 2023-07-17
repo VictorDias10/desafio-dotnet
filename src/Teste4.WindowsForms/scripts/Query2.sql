6d7014c938062302f0a947b1f67811825306c105
Victor <vdias201313@gmail.com>
2023-07-17 00:16:16 -0300

    feat: teste9

---
+++ src/Teste4.WindowsForms/scripts/Query2.sql
@@ -0,0 +1,13 @@
+﻿SELECT 
+	nome as 'Nome do Aluno', 
+	sum(notaPrimeiroSemestre) as 'Nota do Primeiro Semestre', 
+    sum(notaSegundoSemestre) as 'Nota do Segundo Semestre', 
+    avg(notaPrimeiroSemestre + notaSegundoSemestre) as media_anual,
+    ano as 'Ano',
+    CASE
+		WHEN avg(notaPrimeiroSemestre + notaSegundoSemestre) < 6.5 THEN  'Reprovado'
+        ELSE 'Aprovado'
+	END as Status
+FROM escola.alunos 
+GROUP BY nome, ano
+HAVING media_anual > 6;
