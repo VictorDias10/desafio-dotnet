08d439eb0c661b8abb1fd644ff0c253ae801691d
Victor <vdias201313@gmail.com>
2023-07-16 23:35:33 -0300

    feat: teste8

---
+++ src/Teste4.WindowsForms/scripts/Query1.sql
@@ -0,0 +1,12 @@
+﻿SELECT 
+	nome as 'Nome do Aluno', 
+	sum(notaPrimeiroSemestre) as 'Nota do Primeiro Semestre', 
+    sum(notaSegundoSemestre) as 'Nota do Segundo Semestre', 
+    avg(notaPrimeiroSemestre + notaSegundoSemestre) as 'Média Anual',
+    ano as 'Ano',
+    CASE
+		WHEN  avg(notaPrimeiroSemestre + notaSegundoSemestre) < 6.5 THEN  'Reprovado'
+        ELSE 'Aprovado'
+	END as Status
+FROM escola.alunos
+GROUP BY nome, ano;
