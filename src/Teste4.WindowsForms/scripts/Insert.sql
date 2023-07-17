1bf515e90b13c06769763c9332050b3181684996
Victor <vdias201313@gmail.com>
2023-07-16 23:01:49 -0300

    feat: teste7

---
+++ src/Teste4.WindowsForms/scripts/Insert.sql
@@ -0,0 +1,17 @@
+﻿insert into `escola`.`alunos` (`nome`, `notaPrimeiroSemestre`, `notaSegundoSemestre`, `ano`) 
+values ('João', '5', '10', 2022);
+
+insert into `escola`.`alunos` (`nome`, `notaPrimeiroSemestre`, `notaSegundoSemestre`, `ano`) 
+values ('João', '7', '5', 2023);
+
+insert into `escola`.`alunos` (`nome`, `notaPrimeiroSemestre`, `notaSegundoSemestre`, `ano`) 
+values ('João', '3', '7', 2022);
+
+insert into `escola`.`alunos` (`nome`, `notaPrimeiroSemestre`, `notaSegundoSemestre`, `ano`) 
+values ('João', '2', '10', 2023);
+
+insert into `escola`.`alunos` (`nome`, `notaPrimeiroSemestre`, `notaSegundoSemestre`, `ano`) 
+values ('Vinicius', '4.5', '6', 2022);
+
+insert into `escola`.`alunos` (`nome`, `notaPrimeiroSemestre`, `notaSegundoSemestre`, `ano`) 
+values ('Vinicius', '3.1', '7.7', 2022);
